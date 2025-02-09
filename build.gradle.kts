3658c042a4d2155da300fdf08ec98c53fe35d742
Luke Clement <masterclement@gmail.com>
2025-02-09 11:57:36 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.4.2"
+}
+
+group = "org.lukario"
+version = "0.0-TEST"
+
+apply(plugin = "io.spring.dependency-management")
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
